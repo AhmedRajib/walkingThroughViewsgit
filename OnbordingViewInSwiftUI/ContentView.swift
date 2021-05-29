78d4b41d0bad3119188ac39215d9ea1eb8a86bbd
Ahmed Rajib <ahmedrajibbb@gmail.com>
2021-05-29 21:31:23 +0600

    Initial Commit

---
+++ OnbordingViewInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnbordingViewInSwiftUI
+//
+//  Created by MacBook Pro on 29/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnbordingViewInSwiftUI/OnbordingViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnbordingViewInSwiftUIApp.swift
+//  OnbordingViewInSwiftUI
+//
+//  Created by MacBook Pro on 29/5/21.
+//
+
+import SwiftUI
+
+@main
+struct OnbordingViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
